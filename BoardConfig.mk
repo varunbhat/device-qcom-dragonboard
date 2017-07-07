efab7c5628aaecb602a546692db20ee33ee38491
Varun Bhat <varun.bhat@synaptics.com>
2017-07-07 15:04:37 -0700

    pulled from generic

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,15 +17,20 @@
 # Use the non-open-source parts, if they're present
 -include vendor/qcom/dragonboard/BoardConfigVendor.mk
 
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_KERNEL := true
+
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
-TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 := 
-
 TARGET_CPU_VARIANT := generic
+TARGET_CPU_ABI := arm64-v8a
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_VARIANT := cortex-a15
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a53
+
+TARGET_USES_64_BIT_BINDER := true
+
+PRODUCT_PACKAGE_OVERLAYS := device/qcom/dragonboard/overlay
